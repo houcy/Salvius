e6f154eb09872426f976dab613db64361f5caa32
gunther <gunther@salvius.(none)>
2013-07-11 18:28:30 -0400

    setup sets user permission

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ printf "Removing empty download directory"
 
 sudo rm -r Salvius
 
+printf "Giving permissions to current user"
+
+sudo chown $USER -R ../www
+
 printf "Updating package cache...\r"
 
 sudo apt-get update
